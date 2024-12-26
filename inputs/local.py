36653eeeccd344c0e91c00681c6036584a168484
dmnunez1993 <dmnunez1993@gmail.com>
2024-12-25 23:29:10 -0300

    added local backup support

--- inputs/local.py
+++ inputs/local.py
@@ -10,6 +10,7 @@ from .base import BaseInput
 class LocalInput(BaseInput):
 
     def __init__(self, name: str, path: str):
+        super().__init__(name)
         self._name = name
         self._path = path
         self._logger = logging.getLogger("automated_backup_utilities")
@@ -25,3 +26,13 @@ class LocalInput(BaseInput):
         output_folder = mkdtemp()
         now = datetime.now(timezone.utc)
         now_str = now.strftime("%Y-%m-%d_%H:%M:%S")
+        filename_tar_gz = f"{name}_{now_str}.tar.gz"
+        output_path_tar_gz = os.path.join(output_folder, filename_tar_gz)
+
+        command = f"""cd {self._path} \
+             && tar zcvf {output_path_tar_gz} .
+        """
+
+        call(command, shell=True, stdout=DEVNULL, stderr=STDOUT)
+
+        return output_path_tar_gz
